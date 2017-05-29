0c6a5be7ce0986bed32d6f62577ff51b4ff43654
Admin User <admin@Hamids-iMac-3.local>
2017-05-29 13:34:36 +0400

    tab 2017/5/29 13:34

---
+++ mynetfriends/CapacityViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CapacityViewController.swift
+//  mynetfriends
+//
+//  Created by Admin User on 5/29/17.
+//  Copyright © 2017 Farid. All rights reserved.
+//
+
+import UIKit
+
+class CapacityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- mynetfriends/StatusViewController.swift
+++ mynetfriends/StatusViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class StatusViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
