e8933864dd956e36851a0976f02467306b6d5709
Admin User <admin@hamids-imac-3.local>
2017-05-29 11:05:22 +0400

    Initial Commit

---
+++ mynetfriends/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  mynetfriends
+//
+//  Created by Admin User on 5/29/17.
+//  Copyright © 2017 Farid. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
