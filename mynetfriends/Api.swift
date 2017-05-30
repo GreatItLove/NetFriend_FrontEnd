765dc196a44621c0ac51fe3583b754af62629174
Admin User <admin@Hamids-iMac-3.local>
2017-05-31 01:51:40 +0400

    subjectlist

---
+++ mynetfriends/Api.swift
@@ -0,0 +1,9 @@
+//
+//  Api.swift
+//  mynetfriends
+//
+//  Created by Admin User on 5/30/17.
+//  Copyright © 2017 Farid. All rights reserved.
+//
+
+import Foundation
---
+++ mynetfriends/SubjectModel.swift
@@ -0,0 +1,9 @@
+//
+//  SubjectModel.swift
+//  mynetfriends
+//
+//  Created by Admin User on 5/30/17.
+//  Copyright © 2017 Farid. All rights reserved.
+//
+
+import Foundation
